170e1d59a8760cc25f4eeef6a983f60a8f8f3a9a
Asad Khasanov <asadbek1khasanov@gmail.com>
2021-11-22 11:31:31 +0500

    some DP example

---
+++ Tests/Algorithms/CutRodTests.cs
@@ -0,0 +1,36 @@
+using System;
+using Algorithms;
+using NUnit.Framework;
+
+namespace Tests.Algorithms
+{
+    public class CutRodTests
+    {
+        [Test]
+        public void DivideAndConquerTest()
+        {
+            var prices = new int[] { 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
+            var result = CutRod.DivideAndConquer(prices, 4);
+
+            Assert.AreEqual(result, 20);
+        }
+
+        [Test]
+        public void DynamicProgramming_TopDown()
+        {
+            var prices = new int[] { 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
+            var result = CutRod.DynamicProgrammingTopDown(prices, 4);
+
+            Assert.AreEqual(result, 20);
+        }
+
+        [Test]
+        public void DynamicProgramming_BottomUp()
+        {
+            var prices = new int[] { 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
+            var result = CutRod.DynamicProgrammingBottomUp(prices, 4);
+
+            Assert.AreEqual(result, 20);
+        }
+    }
+}
