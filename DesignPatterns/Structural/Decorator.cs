276bc1378e353aa873c8a365bc9d26ee5d670c42
Asad Khasanov <asadbek1khasanov@gmail.com>
2021-11-23 23:36:59 +0500

    added Decorator Pattern implementation

---
+++ DesignPatterns/Structural/Decorator.cs
@@ -0,0 +1,94 @@
+﻿/**
+ * GoF DEFINITON:
+ * Attach additional responsibilities to an object dynamically. Decorators provide a flexible 
+ * alternative to subclassing for extending functionality.
+ *
+ * CONCEPT:
+ * From the GoF definition, it is evident that this pattern uses an alternative to subclassing 
+ * (i.e., inheritance). If inheritance is not allowed, how do you proceed? Yes, you guessed it 
+ * right. It prescribes you to use composition instead of inheritance.
+ * By following the SOLID principle, this pattern promotes the concept where your 
+ * class is closed for modification but open for extension. Using this pattern, you can add
+ * special functionality to a specific object without altering the underlying class.
+ * A decorator is just like a wrapper (or topping) that surrounds the original object 
+ * and adds additional functionality to it. This is why the Decorator pattern is also called a 
+ * Wrapper pattern. This pattern is most effective when you add decorators dynamically. 
+ * Since decorators are often added dynamically, it’s perfectly fine if you do not want them 
+ * in a later phase of development, because the original object may still work.
+ */
+
+using System;
+
+namespace DesignPatterns.Structural
+{
+    public abstract class AbstractHouse
+    {
+        public int AdditionalValue { get; set; }
+        public abstract string MakeHouse();
+    }
+
+    public class ConcreteHouse : AbstractHouse
+    {
+        public ConcreteHouse() { }
+
+        public override string MakeHouse()
+        {
+            return "House is constructed. It's price is: $10,000";
+        }
+    }
+
+    public abstract class AbstractHouseDecorator : AbstractHouse
+    {
+        protected AbstractHouse House;
+
+        public AbstractHouseDecorator(AbstractHouse house)
+        {
+            House = house;
+        }
+
+        public override string MakeHouse()
+        {
+            return House.MakeHouse();
+        }
+    }
+
+    public class FloorDecorator : AbstractHouseDecorator
+    {
+        public FloorDecorator(AbstractHouse house) : base(house)
+        {
+            this.AdditionalValue = 2500;
+        }
+
+        public override string MakeHouse()
+        {
+            var result = this.House.MakeHouse();
+            result = String.Concat(result, "\n", AddFloor());
+            return result;
+        }
+
+        public string AddFloor()
+        {
+            return $" -- Added one more floor. Pay additional {this.AdditionalValue}";
+        }
+    }
+    
+    public class PaintDecorator : AbstractHouseDecorator
+    {
+        public PaintDecorator(AbstractHouse house) : base(house)
+        {
+            this.AdditionalValue = 1000;
+        }
+
+        public override string MakeHouse()
+        {
+            var result = this.House.MakeHouse();
+            result = String.Concat(result, "\n", AddFloor());
+            return result;
+        }
+
+        public string AddFloor()
+        {
+            return $"    --- Repainted the house. Pay additional {this.AdditionalValue}";
+        }
+    }
+}
---
+++ Tests/DesignPatterns/Structural/DecoratorTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DesignPatterns.Structural;
+using NUnit.Framework;
+
+namespace Tests.DesignPatterns.Structural
+{
+    public class DecoratorTests
+    {
+        [Test]
+        public void Decorator_Check_Additional_Dynamic_Features()
+        {
+            const string expectedOutput =
+                "House is constructed. It's price is: $10,000\nHouse is constructed. It's price is: $10,000\n -- Added one more floor. Pay additional 2500\nHouse is constructed. It's price is: $10,000\n -- Added one more floor. Pay additional 2500\n    --- Repainted the house. Pay additional 1000";
+            
+            
+            AbstractHouse house = new ConcreteHouse();
+            string result = house.MakeHouse();
+
+            house = new FloorDecorator(house);
+            result = String.Concat(result, "\n", house.MakeHouse());
+
+            house = new PaintDecorator(house);
+            result = String.Concat(result, "\n", house.MakeHouse());
+            // TestContext.WriteLine(result);
+
+            Assert.AreEqual(expectedOutput, result);
+        }
+    }
+}
