4f1ab8d8a20faa4f8a1bdb3f5b896e726a9fc39e
Asad Khasanov <asadbek1khasanov@gmail.com>
2021-11-24 11:02:19 +0500

    added Proxy Pattern implementation

---
+++ DesignPatterns/Structural/Proxy.cs
@@ -0,0 +1,63 @@
+﻿/**
+ * GoF DEFINITION:
+ * Provide a surrogate or placeholder for another object to control access to it.
+ * 
+ * CONCEPT:
+ * You need to support this kind of design because, in many situations, direct 
+ * communication with an original object is not always possible. This is due to many 
+ * factors, including security and performance issues, resource constraints, the final 
+ * product is in the development phase, and so forth. Proxies can be of different types, 
+ * but fundamentally it is a substitute (or a placeholder) for an original object. As a result, 
+ * when a client interacts with a proxy object, it appears that it is directly talking to the 
+ * actual object. So, using this pattern, you may want to use a class that can perform as an 
+ * interface to the original one.
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Structural
+{
+    public interface ILoginService
+    {
+        public string Login();
+    }
+
+    public class ConcreteLoginService : ILoginService
+    {
+        public string Login()
+        {
+            // Returning a login token
+            return Guid.NewGuid().ToString();
+        }
+    }
+
+    public class LoginServiceProxy : ILoginService
+    {
+        private ILoginService _loginService;
+        private List<string> _users = new List<string>(new string[] { "fredrick", "alejandro", "alfonso", "dominic" });
+        
+        public string CurrentUser { get; set; }
+
+        public LoginServiceProxy(string currentUser)
+        {
+            _users.AddRange(new string[] { "fredrick", "alejandro", "alfonso", "dominic" });
+            CurrentUser = currentUser;
+        }
+
+        public string Login()
+        {
+            if (_users.Contains(CurrentUser))
+            {
+                if (_loginService is null)
+                {
+                    _loginService = new ConcreteLoginService();
+                }
+
+                return _loginService.Login();
+            }
+
+            return String.Empty;
+        }
+    }
+}
---
+++ Tests/DesignPatterns/Structural/ProxyTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using DesignPatterns.Structural;
+using NUnit.Framework;
+
+namespace Tests.DesignPatterns.Structural
+{
+    [TestFixture]
+    public class ProxyTests
+    {
+        [Test]
+        public void Proxy_Should_Login_Existing_User()
+        {
+            const string validUsername = "fredrick";
+            ILoginService loginService = new LoginServiceProxy(validUsername);
+            
+            var result = loginService.Login();
+            
+            Assert.IsNotEmpty(result);
+        }
+        
+        [Test]
+        public void Proxy_Should_Not_Login_Non_Existing_User()
+        {
+            const string invalidUsername = "malhotra";
+            ILoginService loginService = new LoginServiceProxy(invalidUsername);
+
+            var result = loginService.Login();
+            
+            Assert.IsEmpty(result);
+        }
+    }
+}
