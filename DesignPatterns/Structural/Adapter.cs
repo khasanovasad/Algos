7bcc7faa7983806d1a554241840d6d8461cd3c5e
Asad Khasanov <asadbek1khasanov@gmail.com>
2021-11-24 17:12:49 +0500

    added Adapter Pattern implementation

---
+++ DesignPatterns/Structural/Adapter.cs
@@ -0,0 +1,97 @@
+﻿/**
+ * GoF DEFINITION:
+ * Convert the interface of a class into another interface client’s expect. Adapter lets classes 
+ * work together that otherwise could not because of incompatible interfaces.
+ * 
+ * CONCEPT:
+ * From the GoF definition, you can guess that this pattern deals with at least two 
+ * incompatible inheritance hierarchies. In a domain-specific system, the clients are 
+ * habituated on how to invoke methods in software. Those methods can follow an 
+ * inheritance hierarchy. Now assume that you need to upgrade your system and need to 
+ * implement a new inheritance hierarchy. When you do that, you do not want to force your 
+ * clients to learn the new way to access the software. So, what can you do? The solution is 
+ * simple: you write an adapter that accepts client requests and translates these requests 
+ * in a form that the methods in the new hierarchy can understand. As a result, clients can 
+ * enjoy the updated software without any hassle.
+ */
+
+using System;
+
+namespace DesignPatterns.Structural
+{
+    public interface IRectangle
+    {
+        public double CalculateArea();
+        public string AboutMe();
+    }
+    
+    public class Rectangle : IRectangle
+    {
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public double Length { get; set; }
+        public double Width { get; set; }
+        
+        public double CalculateArea()
+        {
+            return Width * Length;
+        }
+
+        public string AboutMe()
+        {
+            return "Actually, this is a rectangle.";
+        }
+    }
+    
+    public interface ITriangle
+    {
+        public double CalculateAreaOfTriangle();
+        public string AboutThisTriangle();
+    }
+    
+    public class Triangle : ITriangle
+    {
+        public Triangle(double height, double baseLength)
+        {
+            Height = height;
+            BaseLength = baseLength;
+        }
+
+        public double Height { get; set; }
+        public double BaseLength { get; set; }
+        
+        public double CalculateAreaOfTriangle()
+        {
+            return 0.5 * Height * BaseLength;
+        }
+
+        public string AboutThisTriangle()
+        {
+            return "Actually, this is a triangle.";
+        }
+    }
+
+    public class RectangleAdapter : IRectangle
+    {
+        private ITriangle _triangle;
+
+        public RectangleAdapter(ITriangle triangle)
+        {
+            _triangle = triangle;
+        }
+        
+        public double CalculateArea()
+        {
+            return _triangle.CalculateAreaOfTriangle();
+        }
+
+        public string AboutMe()
+        {
+            return _triangle.AboutThisTriangle();
+        }
+    }
+}
---
+++ Tests/DesignPatterns/Structural/AdapterTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using DesignPatterns.Structural;
+
+namespace Tests.DesignPatterns.Structural
+{
+    [TestFixture]
+    public class AdapterTests
+    {
+        [Test]
+        public void Adapter_Should_Fit_Triangles_In_Place_Of_Rectangles_When_Needed()
+        {
+            const int maxNumberOfRectangles = 5;
+            const double areaToCover = 100000;
+            const double hundredKMs = 100;
+
+            var rectangles = new List<IRectangle>();
+            double totalAreCovered = 0;
+            int currentNumberOfRectangles = 0;
+
+            while (totalAreCovered != areaToCover)
+            {
+                if (currentNumberOfRectangles < maxNumberOfRectangles)
+                {
+                    var rectangle = new Rectangle(length: hundredKMs, width: hundredKMs);
+                    rectangles.Add(rectangle);
+                    
+                    totalAreCovered += rectangle.CalculateArea();
+                    ++currentNumberOfRectangles;
+                }
+                else
+                {
+                    var triangle = new Triangle(baseLength: hundredKMs, height: hundredKMs);
+                    IRectangle rectangleAdapter = new RectangleAdapter(triangle);
+                    rectangles.Add(rectangleAdapter);
+                    
+                    totalAreCovered += rectangleAdapter.CalculateArea();
+                }
+            }
+
+            // var stringBuilder = new StringBuilder();
+            // foreach (var rectangle in rectangles)
+            // {
+            //     stringBuilder.AppendFormat("{0}\n", rectangle.AboutMe());
+            // }
+            // TestContext.WriteLine(stringBuilder.ToString());
+            
+            Assert.AreEqual(areaToCover, totalAreCovered);
+        }
+    }
+}
