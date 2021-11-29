2901308bdcd71cefa1db6abf3394fae4b2530823
Asad Khasanov <asadbek1khasanov@gmail.com>
2021-11-29 16:39:14 +0500

    added Template Method Pattern implementation

---
+++ DesignPatterns/Behavioural/TemplateMethod.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace DesignPatterns.Behavioural
+{
+    public abstract class TeaService
+    {
+        public string BoilWater()
+        {
+            return "1. Water is boiled.";
+        }
+
+        public abstract string SpecialStep();
+
+        public string Serve()
+        {
+            return "3. Serve the beverage.";
+        }
+
+        public string DisplayAllTheSteps()
+        {
+            var strBuilder = new StringBuilder();
+            strBuilder.Append(BoilWater()).Append('\n');
+            strBuilder.Append(SpecialStep()).Append('\n');
+            strBuilder.Append(Serve()).Append('\n');
+            return strBuilder.ToString();
+        }
+    }
+
+    public class BlackTeaService : TeaService
+    {
+        public override string SpecialStep()
+        {
+            return "2. Add black tea.";
+        }
+    }
+
+    public class GreenTeaService : TeaService
+    {
+        public override string SpecialStep()
+        {
+            return "2. Add green tea.";
+        }
+    }
+}
