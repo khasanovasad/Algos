5f8efa15bedab3d4e99574560b0c0b80e236b385
Asad Khasanov <asadbek1khasanov@gmail.com>
2021-12-06 12:35:53 +0500

    - .

---
+++ Algorithms/LeetCode/Easy/283.MoveZeroes.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Algorithms.LeetCode.Easy
+{
+    public sealed partial class Solution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int idx = 0;
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                if (nums[i] != 0)
+                {
+                    (nums[idx], nums[i]) = (nums[i], nums[idx]);
+                    ++idx;
+                }
+            }
+        }
+    }
+}
---
+++ Algorithms/LeetCode/Easy/704.BinarySearch.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+namespace Algorithms.LeetCode.Easy
+{
+    public sealed partial class Solution
+    {
+        public int Search(int[] nums, int target)
+        {
+            int left = 0;
+            int count = 0;  
+            int right = nums.Length;
+            int mid = (left + right) / 2;
+            
+            if (nums[0] == target) 
+            {
+                return 0;
+            }
+            
+            while (target != nums[mid] && nums.Length / 2 >= count)
+            {
+                if (target > nums[mid]) 
+                {
+                    left = mid;
+                }
+                else
+                {
+                    right = mid;
+                }
+                mid = (left + right) / 2;
+                ++count;
+            }
+            
+            if (count > nums.Length / 2)
+            {
+                return -1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+    }
+}
