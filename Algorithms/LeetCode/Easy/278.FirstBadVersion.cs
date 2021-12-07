96d6ecf3c37f9384baeb1a6b4ce4654c9f0e8f85
Asadbek Khasanov <asadbek1khasanov@gmail.com>
2021-12-07 11:55:49 +0500

    .

---
+++ Algorithms/LeetCode/Easy/278.FirstBadVersion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Algorithms.LeetCode.Easy
+{
+    public sealed partial class Solution
+    {
+        private static bool IsBadVersion(int num)
+        {
+            const int firstBadVersion = 4;
+            return num > firstBadVersion;
+        }
+
+        public int FirstBadVersion(int num)
+        {
+            int left = 0;
+            int right = num;
+
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                if (IsBadVersion(mid))
+                {
+                    right = mid;
+                    continue;
+                }
+
+                left = mid + 1;
+            }
+
+            return left;
+        }
+    }
+}
