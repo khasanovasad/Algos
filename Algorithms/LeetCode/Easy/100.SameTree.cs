bea7b67fd7205094d90858f217e6b92d036c5bc6
Asad Khasanov <asadbek1khasanov@gmail.com>
2021-12-11 16:33:37 +0500

    fix typo

--- Algorithms/LeetCode/Easy/100.SameTree.cs
+++ Algorithms/LeetCode/Easy/100.SameTree.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Algorthms.LeetCode.Easy;
+namespace Algorithms.LeetCode.Easy;
 
 /// Defition of a binary tree
 public class TreeNode
