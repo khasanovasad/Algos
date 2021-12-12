31fdac29269169406060fbe0c6ab824c0bde0d75
Asad Khasanov <asadbek1khasanov@gmail.com>
2021-12-12 23:03:09 +0500

    added 0/1 knapsack solution using DP

---
+++ Algorithms/Misc/ZeroOneKnapsack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Algorithms.Misc;
+
+public static class ZeroOneKnapsack
+{
+    public static double Calculate(int[] values, int[] weights, int capacity)
+    {
+        if (values is null || weights is null || values.Length != weights.Length || capacity < 0)
+        {
+            throw new ArgumentException();
+        }
+
+        int N = values.Length;
+        
+        // creating table
+        var table = new int[N + 1, capacity + 1];
+
+        for (int i = 1; i <= N; ++i)
+        {
+            int w = weights[i - 1], v = values[i - 1];
+            
+            for (int j = 1; j <= capacity; ++j)
+            {
+                table[i, j] = table[i - 1, j];
+
+                if (j >= w && table[i - 1, j - w] + v > table[i, j])
+                {
+                    table[i, j] = table[i - 1, j - w] + v;
+                }
+            }
+        }
+
+        return table[N, capacity];
+    }
+}
---
+++ Tests/Algorithms/Misc/ZeroOneKnapsackTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Algorithms.Misc;
+using NUnit.Framework;
+
+namespace Tests.Algorithms.Misc;
+
+[TestFixture]
+public class ZeroOneKnapsackTests
+{
+    [Test]
+    public void Calculate_Should_When()
+    {
+        int capacity = 6;
+        int[] V = { 30, 20, 100, 90, 160 };
+        int[] W = { 2, 1, 2, 4, 3 };
+
+        var result = ZeroOneKnapsack.Calculate(V, W, capacity);
+        
+        Assert.AreEqual(280, result);
+    }
+}
