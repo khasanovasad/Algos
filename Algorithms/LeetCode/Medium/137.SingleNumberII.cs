631fc1b50a2b7855913aa9dd247190d0b3be1219
Asad Khasanov <asadbek1khasanov@gmail.com>
2021-12-14 01:13:43 +0500

    added solution for 137th problem on leetcode

---
+++ Algorithms/LeetCode/Medium/137.SingleNumberII.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.LeetCode.Medium;
+
+public sealed partial class Solution
+{
+    // O (n)time | O(n) space
+    public int SingleNumber(int[] nums)
+    {
+        var table = new Dictionary<int, int>();
+
+        foreach (var num in nums)
+        {
+            if (!table.ContainsKey(num))
+            {
+                table.Add(num, 1);
+                continue;
+            }
+
+            ++table[num];
+        }
+
+        return table.FirstOrDefault(item => item.Value == 1).Key;
+    }
+}
