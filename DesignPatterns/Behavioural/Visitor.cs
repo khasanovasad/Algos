7c70fb42c03c42e6b9a1ff1cc49684cfc7464c2f
Asadbek Khasanov <asadbek1khasanov@gmail.com>
2021-12-20 15:37:51 +0500

    added Visitor Pattern implementation

---
+++ DesignPatterns/Behavioural/Visitor.cs
@@ -0,0 +1,126 @@
+﻿/**
+ * GoF DEFINITION:
+ * Represent an operation to be performed on the elements of an object structure. Visitor 
+ * lets you define a new operation without changing the classes of the elements on which it 
+ * operates.
+ * 
+ * CONCEPT:
+ * In this pattern, you separate an algorithm from an object structure. So, you can add 
+ * new operations on objects without modifying their existing architecture. This pattern 
+ * supports the open/close principle (which says the extension is allowed, but modification 
+ * is disallowed for entities such as class, function, and so on).*
+ */
+
+using System;
+
+namespace DesignPatterns.Behavioural;
+
+public interface IShape
+{
+    void Move(int x, int y);
+    void Draw();
+    string Accept(IVisitor visitor);
+}
+
+public class Dot : IShape
+{
+    public void Move(int x, int y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Draw()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Accept(IVisitor visitor)
+    {
+        return visitor.VisitDot(this);
+    }
+}
+
+public class Circle : IShape
+{
+    public void Move(int x, int y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Draw()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Accept(IVisitor visitor)
+    {
+        return visitor.VisitCircle(this);
+    }
+}
+
+public class Rectangle : IShape
+{
+    public void Move(int x, int y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Draw()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Accept(IVisitor visitor)
+    {
+        return visitor.VisitRectangle(this);
+    }
+}
+
+public class Triangle : IShape
+{
+    public void Move(int x, int y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Draw()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Accept(IVisitor visitor)
+    {
+        return visitor.VisitTriangle(this);
+    }
+}
+
+public interface IVisitor
+{
+    string VisitDot(Dot dot);
+    string VisitCircle(Circle circle);
+    string VisitRectangle(Rectangle rectangle);
+    string VisitTriangle(Triangle triangle);
+}
+
+public class XMLExportVisitor : IVisitor
+{
+    public string VisitDot(Dot dot)
+    {
+        return "Visited the dot shape.";
+    }
+
+    public string VisitCircle(Circle circle)
+    {
+        return "Visited the circle shape.";
+    }
+
+    public string VisitRectangle(Rectangle rectangle)
+    {
+        return "Visited the rectangle shape.";
+    }
+
+    public string VisitTriangle(Triangle triangle)
+    {
+        return "Visited the triangle shape.";
+    }
+}
---
+++ Tests/DesignPatterns/Behavioural/VisitorTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using NUnit.Framework;
+using DesignPatterns.Behavioural;
+
+namespace Tests.DesignPatterns.Behavioural;
+
+[TestFixture]
+public class VisitorTests
+{
+    [Test]
+    public void Visitor_Should_When()
+    {
+        const string expectedResult = "Visited the dot shape.\tVisited the circle shape.\tVisited the rectangle shape.\tVisited the triangle shape.\t";
+        
+        var strBuilder = new StringBuilder();
+        IVisitor visitor = new XMLExportVisitor();
+        var shapes = new IShape[]
+        {
+            new Dot(),
+            new Circle(),
+            new Rectangle(),
+            new Triangle(),
+        };
+
+        foreach (var shape in shapes)
+        {
+            strBuilder.Append(shape.Accept(visitor)).Append('\t');
+        }
+
+        var result = strBuilder.ToString();
+        
+        Assert.AreEqual(expectedResult, result);
+    }
+}
