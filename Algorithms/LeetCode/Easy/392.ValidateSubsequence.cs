6707df2cd493581ab0fe7caf2c8cd3493abf17dc
Asadbek Khasanov <asad1khasanov@gmail.com>
2022-03-08 23:14:38 +0500

    new problem solved

---
+++ Algorithms/LeetCode/Easy/392.ValidateSubsequence.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.LeetCode.Easy;
+
+public sealed partial class Solution
+{
+    /// O(n) time | O(1) space
+    public bool IsValidSequence(List<int> array, List<int> sequence)
+    {
+        int seqIdx = 0;
+        for (int i = 0; i < array.Count; ++i)
+        {
+            if (seqIdx == sequence.Count)
+            {
+                break;
+            }
+
+            if (array[i] == sequence[seqIdx])
+            {
+                ++seqIdx;
+            }
+        }
+
+        return seqIdx == sequence.Count;
+    }
+}
+
