b8fe50a94edc636128699f18114861c309579adc
Asadbek Khasanov <asad1khasanov@gmail.com>
2022-03-09 22:36:50 +0500

    formatted

--- Algorithms/LeetCode/Easy/977.SquaresOfSortedArray.cs
+++ Algorithms/LeetCode/Easy/977.SquaresOfSortedArray.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Algorithms.LeetCode.Easy;
 
@@ -8,37 +7,37 @@ public sealed partial class Solution
     /// O(n * log(n)) time | O(n) space
     public int[] SortedSquaredArray1(int[] array)
     {
-		var result = new int[array.Length];
+        var result = new int[array.Length];
 
-		for (int i = 0; i < result.Length; ++i)
-		{
-			result[i] = (int) Math.Pow(array[i], 2);
-		}
+        for (int i = 0; i < result.Length; ++i)
+        {
+            result[i] = (int)Math.Pow(array[i], 2);
+        }
 
-		Array.Sort<int>(result);
-		return result;
-	}
+        Array.Sort<int>(result);
+        return result;
+    }
 
     /// O(n) time | O(n) space
     public int[] SortedSquaredArray2(int[] array)
     {
-		var result = new int[array.Length];
-		int start = 0;
-		int end = array.Length - 1;
+        var result = new int[array.Length];
+        int start = 0;
+        int end = array.Length - 1;
 
-		for (int i = result.Length - 1; i >= 0; --i)
+        for (int i = result.Length - 1; i >= 0; --i)
         {
-			if (Math.Abs(array[start]) > Math.Abs(array[end]))
+            if (Math.Abs(array[start]) > Math.Abs(array[end]))
             {
-				result[i] = (int) Math.Pow(array[start], 2);
-				++start;
-				continue;
+                result[i] = (int)Math.Pow(array[start], 2);
+                ++start;
+                continue;
             }
 
-			result[i] = (int) Math.Pow(array[end], 2);
-			--end;
+            result[i] = (int)Math.Pow(array[end], 2);
+            --end;
         }
 
-		return result;
-	}
+        return result;
+    }
 }
